4caf6af28c63d524743438dd0c3687c0da1b79dc
Guilherme88123 <guilherme88123@gmail.com>
2026-06-30 12:52:30 -0300

    Adding wold map

---
+++ Sprites/Tilesets/Barracks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Barracks" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+ <image source="../../../../../../Downloads/Tiny Swords (Free Pack)/Tiny Swords (Free Pack)/Buildings/Red Buildings/Barracks.png" width="192" height="256"/>
+</tileset>
