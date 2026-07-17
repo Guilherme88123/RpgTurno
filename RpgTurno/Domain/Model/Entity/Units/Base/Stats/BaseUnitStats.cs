3f0dd64fac41dd85eadf05009afe82df1b243e37
Guilherme88123 <guilherme88123@gmail.com>
2026-07-16 21:59:09 -0300

    Fix xp needed to level up bug

--- RpgTurno/Domain/Model/Entity/Units/Base/Stats/BaseUnitStats.cs
+++ RpgTurno/Domain/Model/Entity/Units/Base/Stats/BaseUnitStats.cs
@@ -83,7 +83,7 @@ public abstract class BaseUnitStats
 
     private void VerifyLevelUp()
     {
-        while (CurrentExperience > MaxExperience)
+        while (CurrentExperience >= MaxExperience)
         {
             LevelUp();
         }
