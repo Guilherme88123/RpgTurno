eeedd5884488f7882a785663b4c3394ab5c2d533
Guilherme88123 <guilherme88123@gmail.com>
2026-07-22 20:50:06 -0300

    Adding warrior skills sound effects

--- RpgTurno/Domain/Const/Sound/Effect/SoundEffectConst.cs
+++ RpgTurno/Domain/Const/Sound/Effect/SoundEffectConst.cs
@@ -2,5 +2,9 @@
 
 public static class SoundEffectConst
 {
-    public const string TestSoundEffect = "TestSoundEffect";
+    public const string SwordAttack = "SwordAttack";
+    public const string HeavySwordAttack = "HeavySwordAttack";
+    public const string GuardStance = "GuardStance";
+    public const string CleaveSwordAttack = "CleaveSwordAttack";
+    public const string ExecutionSwordAttack = "ExecutionSwordAttack";
 }
