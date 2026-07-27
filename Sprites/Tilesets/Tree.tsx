e767d5f1576c5f03c9331e7cf029221a8378fbc3
Guilherme Doerner de Oliveira <guilherme88123@gmail.com>
2026-07-27 17:50:14 -0300

    Adding trees to tiled world map

---
+++ Sprites/Tilesets/Tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tree" tilewidth="192" tileheight="256" tilecount="8" columns="8">
+ <image source="../Png/Terrain/Trees/Tree_1.png" width="1536" height="256"/>
+</tileset>
