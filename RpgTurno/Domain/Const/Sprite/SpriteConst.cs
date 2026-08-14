8087214ab6e3c4dd324df7ddf6554cbae655f9a1
Guilherme Doerner de Oliveira <guilherme88123@gmail.com>
2026-08-14 16:52:39 -0300

    Adding new enemies to Sprite Const

--- RpgTurno/Domain/Const/Sprite/SpriteConst.cs
+++ RpgTurno/Domain/Const/Sprite/SpriteConst.cs
@@ -2,8 +2,6 @@
 
 public static class SpriteConst
 {
-    #region Entity
-
     #region Unit
 
     #region Ally
@@ -77,6 +75,203 @@ public static class SpriteConst
     #endregion
 
     #region Enemy
+
+    #region Bear
+
+    public const string BearIdle = "Bear_Idle";
+    public const string BearRun = "Bear_Run";
+    public const string BearAttack = "Bear_Attack";
+    public const string BearAvatar = "Bear_Avatar";
+
+    #endregion
+
+    #region Bomb Fish
+
+    public const string BombFishIdle = "BombFish_Idle";
+    public const string BombFishRun = "BombFish_Run";
+    public const string BombFishAttack = "BombFish_Attack";
+    public const string BombFishAvatar = "BombFish_Avatar";
+
+    #endregion
+
+    #region Gnoll
+
+    public const string GnollIdle = "Gnoll_Idle";
+    public const string GnollRun = "Gnoll_Run";
+    public const string GnollAttack = "Gnoll_Attack";
+    public const string GnollHit = "Gnoll_Hit";
+    public const string GnollBone = "Gnoll_Bone";
+    public const string GnollAvatar = "Gnoll_Avatar";
+
+    #endregion
+
+    #region Gnome
+
+    public const string GnomeIdle = "Gnome_Idle";
+    public const string GnomeRun = "Gnome_Run";
+    public const string GnomeAttack = "Gnome_Attack";
+    public const string GnomeAvatar = "Gnome_Avatar";
+
+    #endregion
+
+    #region Harpoon Shark
+
+    public const string HarpoonSharkIdle = "HarpoonShark_Idle";
+    public const string HarpoonSharkRun = "HarpoonShark_Run";
+    public const string HarpoonSharkAttack = "HarpoonShark_Attack";
+    public const string HarpoonSharkHarpoon = "HarpoonShark_Harpoon";
+    public const string HarpoonSharkAvatar = "HarpoonShark_Avatar";
+
+    #endregion
+
+    #region Hex Shaman
+
+    public const string HexShamanIdle = "HexShaman_Idle";
+    public const string HexShamanRun = "HexShaman_Run";
+    public const string HexShamanAttack = "HexShaman_Attack";
+    public const string HexShamanAttack2 = "HexShaman_Attack2";
+    public const string HexShamanAvatar = "HexShaman_Avatar";
+    public const string HexShamanExplosion = "HexShaman_Explosion";
+    public const string HexShamanExplosion2 = "HexShaman_Explosion2";
+    public const string HexShamanProjectile = "HexShaman_Projectile";
+
+    #endregion
+
+    #region Lizard
+
+    public const string LizardIdle = "Lizard_Idle";
+    public const string LizardRun = "Lizard_Run";
+    public const string LizardAttack = "Lizard_Attack";
+    public const string LizardHit = "Lizard_Hit";
+    public const string LizardAvatar = "Lizard_Avatar";
+
+    #endregion
+
+    #region Minotaur
+
+    public const string MinotaurIdle = "Minotaur_Idle";
+    public const string MinotaurRun = "Minotaur_Run";
+    public const string MinotaurAttack = "Minotaur_Attack";
+    public const string MinotaurGuard = "Minotaur_Guard";
+    public const string MinotaurAvatar = "Minotaur_Avatar";
+
+    #endregion
+
+    #region Paddle Shark
+
+    public const string PaddleSharkIdle = "PaddleShark_Idle";
+    public const string PaddleSharkRun = "PaddleShark_Run";
+    public const string PaddleSharkAttack = "PaddleShark_Attack";
+    public const string PaddleSharkRow = "PaddleShark_Row";
+    public const string PaddleSharkAvatar = "PaddleShark_Avatar";
+
+    #endregion
+
+    #region Panda
+
+    public const string PandaIdle = "Panda_Idle";
+    public const string PandaRun = "Panda_Run";
+    public const string PandaAttack = "Panda_Attack";
+    public const string PandaGuard = "Panda_Guard";
+    public const string PandaAvatar = "Panda_Avatar";
+
+    #endregion
+
+    #region Pig
+
+    public const string PigIdle = "Pig_Idle";
+    public const string PigRun = "Pig_Run";
+
+    #endregion
+
+    #region Pig Rider
+
+    public const string PigRiderIdle = "PigRider_Idle";
+    public const string PigRiderRun = "PigRider_Run";
+    public const string PigRiderAttack = "PigRider_Attack";
+
+    #endregion
+
+    #region Skull
+
+    public const string SkullIdle = "Skull_Idle";
+    public const string SkullRun = "Skull_Run";
+    public const string SkullAttack = "Skull_Attack";
+    public const string SkullGuard = "Skull_Guard";
+    public const string SkullAvatar = "Skull_Avatar";
+
+    #endregion
+
+    #region Snake
+
+    public const string SnakeIdle = "Snake_Idle";
+    public const string SnakeRun = "Snake_Run";
+    public const string SnakeAttack = "Snake_Attack";
+    public const string SnakeAvatar = "Snake_Avatar";
+
+    #endregion
+
+    #region Spear Goblin
+
+    public const string SpearGoblinIdle = "SpearGoblin_Idle";
+    public const string SpearGoblinRun = "SpearGoblin_Run";
+    public const string SpearGoblinAttackFast = "SpearGoblin_Attack Fast";
+    public const string SpearGoblinAttackStrong = "SpearGoblin_Attack Strong";
+    public const string SpearGoblinAvatar = "SpearGoblin_Avatar";
+
+    #endregion
+
+    #region Spider
+
+    public const string SpiderIdle = "Spider_Idle";
+    public const string SpiderRun = "Spider_Run";
+    public const string SpiderAttack = "Spider_Attack";
+    public const string SpiderAvatar = "Spider_Avatar";
+
+    #endregion
+
+    #region Thief
+
+    public const string ThiefIdle = "Thief_Idle";
+    public const string ThiefRun = "Thief_Run";
+    public const string ThiefAttack = "Thief_Attack";
+    public const string ThiefAvatar = "Thief_Avatar";
+
+    #endregion
+
+    #region Torch Goblin
+
+    public const string TorchGoblinIdle = "TorchGoblin_Idle";
+    public const string TorchGoblinRun = "TorchGoblin_Run";
+    public const string TorchGoblinAttack = "TorchGoblin_Attack";
+    public const string TorchGoblinAvatar = "TorchGoblin_Avatar";
+
+    #endregion
+
+    #region Troll
+
+    public const string TrollIdle = "Troll_Idle";
+    public const string TrollRun = "Troll_Run";
+    public const string TrollAttack = "Troll_Attack";
+    public const string TrollWindup = "Troll_Windup";
+    public const string TrollRecovery = "Troll_Recovery";
+    public const string TrollClubPart1 = "Troll_ClubPart1";
+    public const string TrollClubPart2 = "Troll_ClubPart2";
+    public const string TrollDead = "Troll_Dead";
+    public const string TrollAvatar = "Troll_Avatar";
+
+    #endregion
+
+    #region Turtle
+
+    public const string TurtleIdle = "Turtle_Idle";
+    public const string TurtleRun = "Turtle_Run";
+    public const string TurtleAttack = "Turtle_Attack";
+    public const string TurtleGuardIn = "Turtle_Guard_In";
+    public const string TurtleGuardOut = "Turtle_Guard_Out";
+    public const string TurtleAvatar = "Turtle_Avatar";
+
+    #endregion
 
     #region Evil Warrior
 
@@ -159,6 +354,8 @@ public static class SpriteConst
     public const string SheepIdle = "Sheep_Idle";
     public const string SheepRun = "Sheep_Run";
     public const string SheepAttack = "Sheep_Attack";
+
+    #endregion
 
     #endregion
 
