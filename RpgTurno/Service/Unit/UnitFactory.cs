62b7ddee880af4a455e017d1576e48102002950f
Guilherme88123 <guilherme88123@gmail.com>
2026-08-17 21:08:29 -0300

    Adding new enemies to unit factory

--- RpgTurno/Service/Unit/UnitFactory.cs
+++ RpgTurno/Service/Unit/UnitFactory.cs
@@ -4,12 +4,32 @@ using Domain.Application.Entity.Units.Ally.Lancer;
 using Domain.Application.Entity.Units.Ally.Pawn;
 using Domain.Application.Entity.Units.Ally.Warrior;
 using Domain.Application.Entity.Units.Base;
+using Domain.Application.Entity.Units.Enemy.Bear;
+using Domain.Application.Entity.Units.Enemy.BombFish;
 using Domain.Application.Entity.Units.Enemy.EvilArcher;
 using Domain.Application.Entity.Units.Enemy.EvilCleric;
 using Domain.Application.Entity.Units.Enemy.EvilLancer;
 using Domain.Application.Entity.Units.Enemy.EvilPawn;
 using Domain.Application.Entity.Units.Enemy.EvilWarrior;
+using Domain.Application.Entity.Units.Enemy.Gnoll;
+using Domain.Application.Entity.Units.Enemy.Gnome;
+using Domain.Application.Entity.Units.Enemy.HarpoonShark;
+using Domain.Application.Entity.Units.Enemy.HexShaman;
+using Domain.Application.Entity.Units.Enemy.Lizard;
+using Domain.Application.Entity.Units.Enemy.Minotaur;
+using Domain.Application.Entity.Units.Enemy.PaddleShark;
+using Domain.Application.Entity.Units.Enemy.Panda;
+using Domain.Application.Entity.Units.Enemy.PigRider;
+using Domain.Application.Entity.Units.Enemy.Sheep;
+using Domain.Application.Entity.Units.Enemy.Skull;
+using Domain.Application.Entity.Units.Enemy.Snake;
+using Domain.Application.Entity.Units.Enemy.SpearGoblin;
+using Domain.Application.Entity.Units.Enemy.Spider;
 using Domain.Application.Entity.Units.Enemy.SupremeWarrior;
+using Domain.Application.Entity.Units.Enemy.Thief;
+using Domain.Application.Entity.Units.Enemy.TorchGoblin;
+using Domain.Application.Entity.Units.Enemy.Troll;
+using Domain.Application.Entity.Units.Enemy.Turtle;
 using Domain.Enum.Unit;
 
 namespace Service.Unit;
@@ -31,6 +51,26 @@ public static class UnitFactory
             UnitCode.SupremeWarrior => new SupremeWarriorEntity(level),
             UnitCode.Pawn => new PawnEntity(level),
             UnitCode.EvilPawn => new EvilPawnEntity(level),
+            UnitCode.Bear => new BearEntity(level),
+            UnitCode.BombFish => new BombFishEntity(level),
+            UnitCode.Gnoll => new GnollEntity(level),
+            UnitCode.Gnome => new GnomeEntity(level),
+            UnitCode.HarpoonShark => new HarpoonSharkEntity(level),
+            UnitCode.HexShaman => new HexShamanEntity(level),
+            UnitCode.Lizard => new LizardEntity(level),
+            UnitCode.Minotaur => new MinotaurEntity(level),
+            UnitCode.PaddleShark => new PaddleSharkEntity(level),
+            UnitCode.Panda => new PandaEntity(level),
+            UnitCode.Turtle => new TurtleEntity(level),
+            UnitCode.Sheep => new SheepEntity(level),
+            UnitCode.PigRider => new PigRiderEntity(level),
+            UnitCode.Skull => new SkullEntity(level),
+            UnitCode.Snake => new SnakeEntity(level),
+            UnitCode.SpearGoblin => new SpearGoblinEntity(level),
+            UnitCode.Spider => new SpiderEntity(level),
+            UnitCode.Thief => new ThiefEntity(level),
+            UnitCode.TorchGoblin => new TorchGoblinEntity(level),
+            UnitCode.Troll => new TrollEntity(level),
 
             _ => throw new ArgumentOutOfRangeException(nameof(unitCode), unitCode, null)
         };
