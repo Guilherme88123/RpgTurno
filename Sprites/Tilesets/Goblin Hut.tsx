9b784620ea83e4b400831f8b5f8f535260f75c4c
Guilherme88123 <guilherme88123@gmail.com>
2026-08-21 22:46:45 -0300

    Adding goblin hut tileset

---
+++ Sprites/Tilesets/Goblin Hut.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Goblin Hut" tilewidth="192" tileheight="256" tilecount="16" columns="16">
+ <image source="../Png/Buildings/Goblin Hut/Goblin Hut.png" width="3072" height="256"/>
+</tileset>
